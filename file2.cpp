ebaccf807d4b3c39f6703686280b0c4cce6aa1dc
anisadzakiyyaha-glitch <anisadzakiyyah.a@gmail.com>
2026-03-09 20:22:45 +0800

    Memperbaiki alur control program

--- file2.cpp
+++ file2.cpp
@@ -20,3 +20,10 @@ int main(){
     cin >> nilBI;
     cout <<"Masukkan nilai matematika = ";
     cin  >> nilMT;
+     rerata = (nilBI+nilMT)/2;
+    if (rerata >= 60)
+    status = "Lolos";
+    else 
+    status = "Gagal";
+    rerata = Rata_rata(nilBI,nilMT);
+    //status = Status_lulus(rerata);
